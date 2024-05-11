f50bd6231b408b3e147e13495335d0f16909efcc
kuneck <kuneck@gmail.com>
2024-05-11 09:47:28 +0100

    Add project files.

---
+++ Branching Assignment/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display welcome message
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            // Prompt user for package weight
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            // Check if weight is greater than 50
+            if (weight > 50)
+            {
+                // Display error message and end program if weight is too heavy
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+
+            // Prompt user for package width
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt user for package height
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt user for package length
+            Console.WriteLine("Please enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate total dimensions
+            int totalSize = width + height + length;
+
+            // Check if dimensions total is greater than 50
+            if (totalSize > 50)
+            {
+                // Display error message and end program if dimensions are too big
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+            // Calculate shipping quote
+            int quote = ((width * height * length) * weight) / 100;
+
+            // Display shipping quote to user
+            Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
+
+        }
+    }
+}
